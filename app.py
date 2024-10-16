9491f33e852d61abc21f264793379fbeaf8048e1
igormat7096 <igornovais.tj96@gmail.com>
2024-10-15 21:33:52 -0300

    atividade13

---
+++ app.py
@@ -0,0 +1,29 @@
+if __name__ == "__main__":
+
+ from calcularadora import exebir_menu
+ from calcularadora import calcular_potencia
+ from calcularadora import calcular_raiz
+
+
+while True:
+     nome = input("Informe seu nome: ")
+     print(f"{nome} o que vc deseja fazer")
+     exebir_menu()
+     opcao = input("Opção desejada: ")
+ 
+     match opcao:
+        
+        case "1":
+            numero1 = int(input("Informe o numero base da potência: ").replace(",","."))
+            numero2 = int(input("Informe o numero expoente da potência: ").replace(",","."))
+            print(calcular_potencia(numero1, numero2))
+            continue
+        case "2":
+            numero3 = int(input("Informe o numero para calcular a raiz quadrada: ").replace(",","."))
+            print(calcular_raiz(numero3))
+            continue
+            
+        case "3":
+            break
+        case _:
+            print("Opção inválida.")
---
+++ calcularadora.py
@@ -0,0 +1,9 @@
+import math
+
+def exebir_menu():
+    print("1 - Calcular potência.")
+    print("2 - Calcular raiz quadrada.") 
+    print("3 - Sair do programa!")
+
+calcular_potencia = lambda b, e: b ** e
+calcular_raiz = lambda numero3: math.sqrt(numero3)
